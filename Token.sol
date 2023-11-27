88b4b3d8d0b834cd69fd7fe0b06c20629dcd6780
Rajivs1525 <145263927+Rajivs1525@users.noreply.github.com>
2023-11-27 22:50:50 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,87 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-contract Token {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-    address public owner;
+import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
 
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowances;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can call this function.");
-        _;
+contract SIGMA is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("SIGMA", "SIG")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 100 * 10 ** decimals());
     }
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply;
-        owner = msg.sender;
-        balanceOf[msg.sender] = _totalSupply;
-    }
-    function transfer(address _to, uint256 _value) external returns (bool) {
-        _transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function approve(address _spender, uint256 _value) external returns (bool) {
-        require(_spender != address(0), "Invalid spender address.");
-        allowances[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
-        require(_from != address(0), "Invalid sender address.");
-        require(_to != address(0), "Invalid recipient address.");
-        require(_value <= balanceOf[_from], "Insufficient balance.");
-        require(_value <= allowances[_from][msg.sender], "Insufficient allowance.");
-
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        allowances[_from][msg.sender] -= _value;
-
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function burn(uint256 _value) external returns (bool) {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance.");
-
-        balanceOf[msg.sender] -= _value;
-        totalSupply -= _value;
-
-        emit Burn(msg.sender, _value);
-        return true;
-    }
-
-    function mint(address _to, uint256 _value) external onlyOwner returns (bool) {
-        require(_to != address(0), "Invalid recipient address.");
-
-        balanceOf[_to] += _value;
-        totalSupply += _value;
-
-        emit Transfer(address(0), _to, _value);
-        return true;
-    }
-
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        require(_to != address(0), "Invalid recipient address.");
-        require(_value <= balanceOf[_from], "Insufficient balance.");
-
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-
-        emit Transfer(_from, _to, _value);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
 
